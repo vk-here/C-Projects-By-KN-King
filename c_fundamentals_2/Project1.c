75ef34a1a25e45c81787794f9df9461784a6b6ee
vk-here <vigneshkumar.co@gmail.com>
2024-11-04 16:06:33 +0530

    Added comments to the programs Made sure to include the details of the project using comments in the programs itself and mentioned the expected output. Completed the first 3 projects.

--- c_fundamentals_2/Project1.c
+++ c_fundamentals_2/Project1.c
@@ -1,10 +1,23 @@
 ﻿#include<stdio.h>
 int main(void)
 {
+	//this was little difficult then i thought it would be
+	// basically you have figure the tab or space that is needed to get the symbol
+	//and to use more than one print to make it easy or you can do it harder which is just counting.
 	printf("        \n       * \n      * \n     * \n");
 	printf("*   *\n");
 	printf(" * *\n");
 	printf("  *\n");
 
 	return 0;
+
+//output 
+/*     *
+      *
+     *
+*   *
+ * *
+  * 	
+	 */
+//the stars in the long comment are not part of it.
 }
---
+++ c_fundamentals_2/Project2 and 3.c
@@ -0,0 +1,21 @@
+﻿#define pi 3.14
+#include<stdio.h>
+int main(void)
+{
+	float volume;
+	//int radius = 10; used for 2nd project
+	int radius;
+
+	printf("Enter the radius of the sphere : ");
+	scanf("%d",&radius);
+	//this two statements satisfy the 3rd project conditions.
+
+	volume = (4.0f/3.0f) * (pi) * (radius * radius * radius) ;
+	printf("vol %f",volume);
+
+	return 0;
+}
+//output
+//answer is 4186.666791
+//this will be the output if we use 4/3 instead of 4.0f/3.0f to 3140.000000.
+//the output shows the difference between use of float in place of int. 
