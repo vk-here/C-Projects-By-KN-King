c155d722498070252873c0475802be6d94515e2a
vk-here <vigneshkumar.co@gmail.com>
2024-11-20 17:41:03 +0530

    Added project 7 and 8 This completes the 2nd chapter exercises

---
+++ c_fundamentals_2/project7.c
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+int main(void){
+	int dollar;
+	printf("\nEnter the amount to know amount of change needed:");
+	scanf("%d",&dollar);
+	int twenty, ten, five, one;
+	twenty = dollar/20;
+	dollar = dollar - (twenty*20);
+    ten = dollar/10;
+	dollar = dollar - (ten*10);
+	five = dollar/5;
+	dollar = dollar - (five*5);
+	one = dollar;
+
+	printf("Twenty : %d\n Ten : %d\n Five : %d\n One : %d\n",twenty,ten,five,one);
+}
+
+//at first I was lost for long time I did not know what to do even after reading the hint
+//I was able to understand the problem and solve it after seeing few solutions 
+//i guess it is important to know how to solve a problem this helped
+//I might be close enough to solve it without seeing the code but still some things i understood
+//tbh this was fun to solve
---
+++ c_fundamentals_2/project8.c
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+#define cal (loan * rate / 100.0 / 12.0)
+int main(void){
+	float loan, rate, payment;
+	printf("\nEnter amount of loan :\n");
+	scanf("%f",&loan);
+	printf("Enter the interest rate :\n");
+	scanf("%f",&rate);
+	printf("Enter the montly payment :\n");
+	scanf("%f",&payment);
+    loan = loan - payment + cal;
+    printf("Balance remaining after first payment: $%.2f\n", loan);
+
+    
+    loan = loan - payment + cal;
+    printf("Balance remaining after second payment: $%.2f\n", loan); 
+
+    loan = loan - payment + cal;
+    printf("Balance remaining after third payment: $%.2f\n", loan);
+
+return 0;
+	}
+//this was hard for me to figure out but i refered to some code
+// done it as i could and used the define to make own function that can be reused
